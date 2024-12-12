27d949675ccce1ad1e9e1dd78f6ff80fa7f885a6
isaacsummers <isaac@datarift.one>
2024-12-12 11:33:54 -0500

    updated project dir

--- plugins/xdg/xdg.plugin.zsh
+++ plugins/xdg/xdg.plugin.zsh
@@ -9,7 +9,7 @@ export XDG_CACHE_HOME=~/.cache
 export XDG_DATA_HOME=~/.local/share
 export XDG_STATE_HOME=~/.local/state
 export XDG_RUNTIME_DIR=~/.xdg
-export XDG_PROJECTS_DIR=~/Projects
+export XDG_PROJECTS_DIR=~/projects
 
 for _xdgdir in XDG_{CONFIG,CACHE,DATA,STATE}_HOME XDG_RUNTIME_DIR; do
   [[ -e ${(P)_xdgdir} ]] || mkdir -p ${(P)_xdgdir}
